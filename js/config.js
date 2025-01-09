1b13f9d458c05da67f7703c54ed9e57fbf66b3b8
Morris08 <89796573+Morris08@users.noreply.github.com>
2025-01-09 12:22:03 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_YbNPOAQW7', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '64oqoui9bifie4ep3h084gaem3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_zD7rNosa0', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1bpl4pfnjp1i7cdvqh6gs2c5bq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
